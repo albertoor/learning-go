3cb52d55442229832ff45756d457f234e7a57bd8
albertoornelesdev <albertoornelasdev@gmail.com>
2021-08-22 13:42:36 -0700

    exploring others packages

--- Chapter1/main.go
+++ Chapter1/main.go
@@ -1,6 +1,14 @@
 package main
-import "fmt"
+
+// import "fmt"
+
+import (
+  "math"
+  "strings"
+)
 
 func main()  {
-	fmt.Println("Hello World")
+	// fmt.Println("Hello World")
+  math.Floor(2.75)
+  strings.Title("Head first go")
 }
