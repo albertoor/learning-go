694878551ba7c45c4619c96d6e4cdc35e0e62bcd
albertoornelesdev <albertoornelasdev@gmail.com>
2021-08-22 16:11:03 -0700

    strings in go

--- Chapter1/main.go
+++ Chapter1/main.go
@@ -11,4 +11,10 @@ func main()  {
 	// fmt.Println("Hello World")
   math.Floor(2.75)
   strings.Title("Head first go")
+
+  // Strings
+  fmt.Println("Hello, GO!")
+  fmt.Println("Hello, \tGO!")
+  fmt.Println("Quotes: \"\"")
+  fmt.Println("Backslash: \\")
 }
